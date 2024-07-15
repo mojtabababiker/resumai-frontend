0b1821c9b9c5f30f934800a2ce2e344877154ee5
mojtabababiker <mojmohammad98@gmail.com>
2024-07-15 08:04:59 +0200

    Update the frontend app utilities, adding the api-clients utility

--- frontend/app/utils/api-client.ts
+++ frontend/app/utils/api-client.ts
@@ -1,1 +1,48 @@
- 
+ // utilities that interact with the FastAPI backend, and return the data in a format that the frontend can use
+
+import axios from "axios";
+
+interface globalFetcherProps {
+    url: string;
+    method: string;
+    token: string;
+    bodyData?: string | object;
+    contentType?: string;
+    
+}
+
+const BASE_API_URL = 'http://localhost:8000/api/v1';
+
+export const apiClient = axios.create({
+  baseURL: BASE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+/**
+ * globalFetcher is a utility function that makes various requests to the FastAPI backend according to the parameters passed to it
+ * @param param0 object that contains all the necessary information to make the request
+ * @returns a promise that resolve to the json data from the response as an object, or an error if the request fails
+ */
+export const globalFetcher = async ({url, method, token, bodyData, contentType='application/json' }: globalFetcherProps) => {
+    try{
+        const {status, data} = await apiClient.request(
+            {
+                url,
+                method,
+                headers: {Authorization: `Bearer ${token}`, 'Content-Type': contentType},
+                data: bodyData
+            }
+        );
+        if (status >= 300 || status < 200) {
+            throw new Error(data.detail);
+        }
+        return data;
+    } catch (error) {
+        console.log(error);
+        // throw new Error(error);
+    }
+}
+
