09a7281bb7b107390528d08eb50154fed8d5ade9
mojtabababiker <mojmohammad98@gmail.com>
2024-07-15 15:08:36 +0200

    general enhancement for the root page and layout, changing the layout and the color plate

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-0 mt-28 md:mt-20 mx-auto md:p-16z xl:m-24">
       {<NavBar user={user}></NavBar>}
       {/* Hero */}
-      <section className="h-[100svh - 60px] md:h-[100svh - 120px] flex flex-col md:flex-row gap-4 py-4 px-5">
+      <section className="h-[100svh - 60px] md:h-[100svh - 120px] flex flex-col md:flex-row gap-4 py-4 px-5 mt-24 md:mt-32 lg:mt-24">
         {/* slogan */}
         <div className="relative w-full md:w-1/2 flex flex-col justify-around items-center gap-4">
           <h1 className="text-6xl text-center md:text-left md:text-6xl lg:text-8xl font-bold">Your Resume, Automated</h1>
