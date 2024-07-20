a683fb0f2319dc4b3e91b445da7b54497880f502
mojtabababiker <mojmohammad98@gmail.com>
2024-07-20 05:54:09 +0200

    Update the dashboard page, Adding user resumes display on the dashboard, and the resumes altering options

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -4,9 +4,13 @@ import Image from 'next/image'
 import { DashboardNavBar } from '@/app/ui/nav_bar';
 import { useUser } from '@/app/utils/auth';
 import Link from 'next/link';
+import { getUserResumes, Resume } from '../utils/resumes-client';
+import { templateProps } from '../ui/resume-templates/interfaces';
 
 export default function DashboardPage() {
     const user = useUser();
+    const [userResumes, setUserResumes]: [{ recent: Resume[], all: Resume[] }, Function] = useState({ recent: [], all: [] });
+    const [resumesNotFetched, setResumesNotFetched] = useState(true);
     const [activeTab, setActiveTab] = useState({ btn_id: 'recent-tab-btn', tab_id: 'recent-tab' });
     const [resumeBuild, setResumeBuild] = useState(false);
     const [templateId, setTemplateId] = useState('');
@@ -41,31 +45,141 @@ export default function DashboardPage() {
         setActiveTab({ btn_id: event.target.id, tab_id: event.target.getAttribute('itemID') });
     }
 
+    const editResume = (resume: Resume, event: BaseSyntheticEvent) => {
+        event.stopPropagation();
+        // call the resume crafting page /dashboard/[resume.templateId] passing the resume.data to it
+        console.log(resume.templateId);
+        console.log(resume.data);
+    }
+
+    const deleteResume = (resumeId: string, event: BaseSyntheticEvent) => {
+        event.stopPropagation();
+        // ask the user to confirm their choice to delete
+        // call the resume api client to delete the resume with id resumeId
+        console.log(resumeId);
+    }
+
+    const downloadResume = (resume: Resume, event: BaseSyntheticEvent) => {
+        event.stopPropagation();
+        // use the j2pdf and html2canvas to convert the resume template into a pdf
+        // download the pdf file name it as <user-full-name-resume>.pdf
+        console.log(resume);
+    }
+
+    const fetchUserResumes = async () => {
+        try {
+            const resumes = await getUserResumes();
+            setUserResumes(resumes);
+            // alert(Object.entries(resumes.at(1)));
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setResumesNotFetched(false);
+        }
+    }
+    resumesNotFetched && fetchUserResumes();
     return (
-        <main className='w-full max-w-screen-xl flex flex-col items-center p-0 md:p-4 mt-28 md:mt-5'>
+        <main className='relative w-full max-w-screen-xl flex flex-col items-center p-0 md:p-0 mt-28 md:mt-5'>
             <DashboardNavBar user={user.user} />
-            <section className='min-w-full mt-20 pb-20 flex flex-col gap-y-10 justify-center items-center md:flex-row md:items-start md:justify-between'>
-                <div className='w-3/4 flex'>
-                    <h1 className='text-4xl md:text-3xl md:font-light text-center md:text-start'>Welcome back <span className='font-semibold'>{user_name}</span>, let's make sure you get it this time</h1>
-                </div>
-                <div className='w-1/4 flex justify-end'>
-                    <button className="text-white bg-[rgb(var(--primary-rgb))] hover:bg-[rgba(var(--primary-light-rgba))]
-                    focus:ring-0 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" onClick={startResumeBuild} tabIndex={0}>
-                        Create
-                        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" strokeWidth="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5" />
-                        </svg>
-                    </button>
-                </div>
-            </section>
-            <section className='min-w-full mb-20 flex flex-col gap-y-10 justify-stretch overflow-hidden border-0 border-b border-[rgba(255, 255, 255, 0.01)] border-opacity-10'>
-                <div className='flex justify-start gap-3 overflow-auto'>
-                    <button className={`${buttonCssClass} ${activeTab.btn_id === 'recent-tab-btn' && 'border-b border-[rgba(var(--primary-light-rgba))] border-opacity-60'}`} id='recent-tab-btn' itemID='recent-tab' onClick={handleTabDisplay}>Recent</button>
-                    <button className={`${buttonCssClass} ${activeTab.btn_id === 'all-tab-btn' && 'border-b border-[rgba(var(--primary-light-rgba))] border-opacity-60'}`} id='all-tab-btn' itemID='all-tab' onClick={handleTabDisplay}>All Resumes(2)</button>
-                </div>
-            </section>
             <section>
+                <div className='w-svw p-0 m-0 bg-no-repeat bg-cover backdrop-brightness-0 backdrop-blur-sm overflow-hidden bg-[url("/bg-4.jpg")]'>
+                    <div className='w-svw bg-[#0000006f] py-4 flex justify-center'>
+                        <div className='max-w-screen-xl w-full mt-20 px-4 pb-20 flex flex-col gap-y-10 justify-center items-center md:flex-row md:items-start md:justify-between'>
+                            <div className='w-3/4 flex text-white'>
+                                <h1 className='text-4xl md:text-3xl md:font-light text-center md:text-start'>Welcome back <span className='font-semibold'>{user_name}</span>, let's make sure you get it this time</h1>
+                            </div>
+                            <div className='w-1/4 flex justify-center md:justify-end '>
+                                <button className="text-white bg-[rgb(var(--primary-rgb))] hover:bg-[rgba(var(--primary-light-rgba))]
+                    focus:ring-0 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" onClick={startResumeBuild} tabIndex={0}>
+                                    Create
+                                    <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" strokeWidth="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <section className='min-w-full  mb-20 flex gap-y-10 justify-center overflow-hidden border-0 border-b border-[rgba(255, 255, 255, 0.01)] border-opacity-10'>
+                    <div className='max-w-screen-xl w-full flex justify-start gap-3 overflow-auto'>
+                        <button className={`${buttonCssClass} ${activeTab.btn_id === 'recent-tab-btn' && 'border-b border-[rgba(var(--primary-light-rgba))] border-opacity-60'}`} id='recent-tab-btn' itemID='recent-tab' onClick={handleTabDisplay}>Recent</button>
+                        <button className={`${buttonCssClass} ${activeTab.btn_id === 'all-tab-btn' && 'border-b border-[rgba(var(--primary-light-rgba))] border-opacity-60'}`} id='all-tab-btn' itemID='all-tab' onClick={handleTabDisplay}>All Resumes({userResumes.all.length})</button>
+                    </div>
+                </section>
+            </section>
+            <section className='overflow-x-hidden'>
                 {/* contains the recent and all resumes tabs */}
+                {activeTab.tab_id === 'recent-tab' && userResumes.recent.length > 0 &&
+                    <div className='flex justify-between gap-3 flex-wrap overflow-x-hidden'>
+                        {userResumes.recent.map((resume, idx) => {
+                            const resumeData = resume.data;
+                            return (
+                                <div key={resume._id} id={resume._id} onClick={(event) => editResume(resume, event)} className="relative w-full md:w-[30%] lg:w-[] p-6 bg-white border border-gray-200 rounded-lg shadow cursor-pointer hover:scale-90 transition-transform ease-in">
+                                    <a href="#">
+                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900">{resumeData.title.jobTitle}</h5>
+                                    </a>
+                                    <p className="mb-3 font-normal text-xs text-gray-700">{resumeData.summary?.slice(0, 140)}...</p>
+                                    <p className="mb-3 font-normal text-xs text-gray-700"><span className='font-semibold'>Last Modification:</span> {resume.updated_at.split('T')[0]}</p>
+                                    <button onClick={(event) => { downloadResume(resume, event) }} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[rgba(var(--primary-light-rgba))] rounded-lg hover:bg-[rgb(var(--primary-rgb))] focus:ring-0 focus:outline-none">
+                                        Download
+                                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg>
+                                    </button>
+                                    <button onClick={(event) => deleteResume(resume._id, event)} className="absolute top-0 right-0 text-gray-400 bg-transparent hover:bg-[rgb(var(--primary-rgb))] hover:text-[rgb(var(--background-start-rgb))] rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+                                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                        </svg>
+                                        <span className="sr-only">Close modal</span>
+                                    </button>
+                                    <button className="absolute top-[2rem] right-0 flex justify-center items-center w-8 h-8 bg-inherit hover:bg-[rgb(var(--primary-rgb))] hover:text-[rgb(var(--background-start-rgb))] rounded-lg" onClick={(event) => editResume(resume, event)}>
+                                        <svg className="w-5 h-5 hover:text-[rgb(var(--background-start-rgb))]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd" d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z" clip-rule="evenodd" />
+                                            <path fill-rule="evenodd" d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z" clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                </div>
+
+                            )
+                        })}
+                    </div>
+                }
+
+                {activeTab.tab_id === 'all-tab' && userResumes.all.length > 0 &&
+                    <div className='flex justify-between gap-3 flex-wrap overflow-x-hidden'>
+                        {userResumes.all.map((resume, idx) => {
+                            const resumeData = resume.data;
+                            return (
+                                <div key={resume._id} id={resume._id} onClick={(event) => { editResume(resume, event) }} className="relative w-full md:w-[30%] lg:w-[] p-6 bg-white border border-gray-200 rounded-lg shadow cursor-pointer hover:scale-90 transition-transform ease-in">
+                                    <a href="#">
+                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900">{resumeData.title.jobTitle}</h5>
+                                    </a>
+                                    <p className="mb-3 font-normal text-xs text-gray-700">{resumeData.summary?.slice(0, 140)}...</p>
+                                    <p className="mb-3 font-normal text-xs text-gray-700"><span className='font-semibold'>Last Modification:</span> {resume.updated_at.split('T')[0]}</p>
+                                    <button onClick={(event) => { downloadResume(resume, event) }} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[rgba(var(--primary-light-rgba))] rounded-lg hover:bg-[rgb(var(--primary-rgb))] focus:ring-0 focus:outline-none">
+                                        Download
+                                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg>
+                                    </button>
+                                    <button onClick={(event) => deleteResume(resume._id, event)} className="absolute top-0 right-0 text-gray-400 bg-transparent hover:bg-[rgb(var(--primary-rgb))] hover:text-[rgb(var(--background-start-rgb))] rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+                                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                        </svg>
+                                        <span className="sr-only">Close modal</span>
+                                    </button>
+                                    <button className="absolute top-[2rem] right-0 flex justify-center items-center w-8 h-8 bg-inherit hover:bg-[rgb(var(--primary-rgb))] hover:text-[rgb(var(--background-start-rgb))] rounded-lg" onClick={(event) => editResume(resume, event)}>
+                                        <svg className="w-5 h-5 hover:text-[rgb(var(--background-start-rgb))]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd" d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z" clip-rule="evenodd" />
+                                            <path fill-rule="evenodd" d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z" clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                </div>
+
+                            )
+                        })}
+                    </div>
+                }
             </section>
 
             {/* The modal for building the resume */}
--- frontend/app/utils/resumes-client.ts
+++ frontend/app/utils/resumes-client.ts
@@ -1,6 +1,16 @@
 // utilities that interact with the FastAPI backend on resumes endpoint, and return the data in a format that the frontend can use
 
 import {globalFetcher} from "@/app/utils/api-client";
+import { templateProps } from "../ui/resume-templates/interfaces";
+
+
+export interface Resume {
+    data: templateProps,
+    _id: string,
+    created_at: string,
+    updated_at: string,
+    templateId:string,
+}
 
 /**
  * a utility that interact with the backend API to create a new resume for the current logged in user
@@ -24,4 +34,31 @@ export async function addResume(resumeObj: object): Promise<{data: string, messa
         }
     );
     return {data: res, message: 'Resume Saved successfully'};
+}
+
+export async function getUserResumes(): Promise<{recent:Resume[], all: Resume[]}> {
+    const tokenObj = localStorage.getItem('auth-token');
+    const token = tokenObj ? JSON.parse(tokenObj) : null;
+
+    if (!token) {
+        throw new Error('Session ended, Login required');
+    }
+
+    const res:Resume[] = await globalFetcher(
+        {
+            url: '/resumes',
+            method: 'get',
+            token,
+        }
+    );
+    res.sort((a, b) => {
+        if (a.updated_at === b.updated_at) {
+            return 0;
+        }
+        if (a.updated_at < b.updated_at) {
+            return 1;
+        }
+        return -1;
+    })
+    return {recent: res.slice(0, 3), all: res}
 }
